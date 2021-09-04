567a162f035453ca5ca92f36c942a5ca0714e6c3
{Utopia300} <{jiyeyouren523@gmail.com}>
2021-09-05 00:42:02 +0900

    a

---
+++ index.js
@@ -0,0 +1,10 @@
+const form = document.getElementById("form");
+
+// 基礎
+// 要素.addEventListener(イベント名, () => {
+//   イベントが起きたときの処理;
+// });
+
+form.onsubmit = () => {
+  form = alert("クリックされました");
+};
